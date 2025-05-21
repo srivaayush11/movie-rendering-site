b3772b4166ccf97d78d7409b8f7bbe8767ae95cc
SRIVASTAVA Aayush <aayush.srivastava@soprasteria.com>
2025-05-21 14:53:06 +0530

    added loader spinner in suspense fallback

--- src/hocs/withLazyLoading.jsx
+++ src/hocs/withLazyLoading.jsx
@@ -1,6 +1,13 @@
 import React, { Suspense, lazy } from "react";
+import netflix_spinner from "../assets/netflix_spinner.gif";
 
-const withLazyLoading = (componentLoader, fallback = <div>Loading...</div>) => {
+const Loader = () => (
+  <div className="login-spinner">
+    <img src={netflix_spinner} alt="" loading="lazy" />
+  </div>
+);
+
+const withLazyLoading = (componentLoader, fallback = <Loader />) => {
   const LazyComponent = lazy(componentLoader);
 
   return (props) => (
